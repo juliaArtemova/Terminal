b9e1c5cbeb8455f63b8c3ed2506e5ccf13a569f1
juliaArtemova <121572764+juliaArtemova@users.noreply.github.com>
2023-05-11 21:16:34 +0300

    script.sh

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd home_1/
+mkdir foldername_1 foldername_2 foldername_3
+cd /e/new_folder/
+touch home_5.txt home_6.txt home_7.txt home_1.json home_2.json
+mkdir foldername_4 foldername_5 foldername_6
+ls -la
+mv home_5.txt home_6.txt home_7.txt /e/homework/
